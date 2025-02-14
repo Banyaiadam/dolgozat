16f7c9b56c43db4dbf2f0f2a0860be2dd241b3f1
Banyaiadam <77103062+Banyaiadam@users.noreply.github.com>
2025-02-14 09:12:18 +0100

    Update BorrowingsController.php
    hetedik

--- app/Http/Controllers/BorrowingsController.php
+++ app/Http/Controllers/BorrowingsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BorrowingsController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
 }
