2a57499dbf662a25b66334d524ccb64cafe3f246
Banyaiadam <77103062+Banyaiadam@users.noreply.github.com>
2025-02-14 08:44:39 +0100

    negyedik

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'user1',
+            'role' => 'user'
+        ]);
+        User::factory()->create([
+            'name' => 'admin1',
+            'role' => 'admin'
         ]);
     }
 }
