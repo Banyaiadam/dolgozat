a92ae5bf05848e415fbb53313828bd0daea6b0f9
Banyaiadam <77103062+Banyaiadam@users.noreply.github.com>
2025-02-14 09:03:02 +0100

    otodik

---
+++ database/seeders/UserBookSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UserBookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+
+        $books = Book::all();
+
+
+        foreach ($users as $user) {
+
+            DB::table('user_book')->insert([
+
+                'user_id' => $user->id,
+
+                'book_id' => $books->random()->id,
+
+            ]);
+
+            DB::table('user_book')->insert([
+
+                'user_id' => $user->id,
+
+                'book_id' => $books->random()->id,
+
+            ]);
+
+            DB::table('user_book')->insert([
+
+                'user_id' => $user->id,
+
+                'book_id' => $books->random()->id,
+
+            ]);
+        }
+    }
+}
